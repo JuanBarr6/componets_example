eb342a7c6f037f5f59ad3b39f2c64fc09949f628
JuanBarr6 <juanm.barrerao06@gmail.com>
2024-03-18 15:21:17 -0500

    feat: add token styles script

---
+++ scripts/build-custom-properties.js
@@ -0,0 +1,30 @@
+import { myDecisions } from "../../../Opi/cero-to-production/components/tokens.js";
+
+import * as fs from "fs";
+
+const transformTokens = (parentKey, object) => {
+  const objectKeys = Object.keys(object);
+
+  return objectKeys.reduce((tokensTransformed, objectKey) => {
+    const value = object[objectKey];
+
+    if (typeof value === "object") {
+      if (parentKey === null)
+        return `${tokensTransformed}\t${transformTokens(objectKey, value)}`;
+
+      return `${tokensTransformed}\t${transformTokens(`${parentKey}-${objectKey}`, value)}`;
+    }
+
+    return `${tokensTransformed}\n\t--${parentKey}-${objectKey}: ${value};`;
+  }, "");
+};
+
+const data = `:root {\n\t${transformTokens(null, myDecisions).trim()}\n}`;
+
+fs.writeFile("./tokens.css", data, "utf8", (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("File written successfully");
+  }
+});
